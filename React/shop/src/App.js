dda2c10c7bee93b0052db7752f0647779c1a414d
Youn-Young-Seon <szdbsyyzs@naver.com>
2021-12-06 17:42:34 +0900

    apple React chap.2 Lifecycle...

--- React/shop/src/App.js
+++ React/shop/src/App.js
@@ -1,17 +1,25 @@
-import { Navbar, Container, Nav, NavDropdown, Jumbotron, Button } from 'react-bootstrap';
+import React, {useState} from 'react';
+import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import './App.css';
+import Data from './data.js';
+import Detail from './detail.js'
+
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
+
+  let [shoes, shoes변경] = useState(Data);
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="detail">Detail</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -23,18 +31,60 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <Jumbotron>
-        <h1>Hello, world!</h1>
+
+    <Switch>
+
+      <Route exact path="/">
+        <Jumbotron></Jumbotron>
+        <div>메인페이지에요</div>
+        <div className="container">
+          <div className="row">
+            <Product shoes={shoes}/>      
+          </div>
+        </div>
+      </Route>
+
+      <Route path="/detail/:id">
+        <Detail shoes={shoes}/>
+      </Route>
+
+      {/* <Route path="/:id">
+        <div>아무거나적었을때 이거 보여주셈</div>
+      </Route> */}
+
+    </Switch>
+
+      {/* <Route path="/어쩌구" component={Modal}></Route> */}
+
+    </div>
+  );
+}
+
+function Product(props){
+  return(
+    props.shoes.map((a, i) => {
+      return <div className="col-md-4" key={i}>
+              <img src={`https://codingapple1.github.io/shop/shoes${i+1}.jpg`} alt="" width="100%"></img>
+              <h4>{a.title}</h4>
+              <p>{a.content} & {a.price}</p>
+             </div>      
+    })
+  )
+}
+
+function Jumbotron(){
+  return(
+    <div className="jumbotron background">
+        <h1>20% Season Off</h1>
         <p>
           This is a simple hero unit, a simple jumbotron-style component for calling
           extra attention to featured content or information.
         </p>
         <p>
           <Button variant="primary">Learn more</Button>
-        </p>
-      </Jumbotron>
+        </p>      
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ React/shop/src/detail.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react"
+import { useHistory, useParams } from "react-router-dom";
+import styled from 'styled-components';
+import './Detail.scss'
+
+
+let 박스 = styled.div`
+padding : 20px;
+`;
+
+let 제목 = styled.h4`
+font-size : 25px;
+color : ${ props => props.색상 };
+`;
+
+// class Detail2 extends React.Component{
+//     componentDidMount(){
+//     }
+//     componentWillUnmount(){
+//     }
+// }
+
+
+function Detail(props){  
+
+    useEffect(() => {
+        let 타이머 = setTimeout(() => {
+            amAlert변경(false);
+        }, 2000)
+
+        return function 어쩌구(){
+            // 실행할코드~~~
+        }
+    });
+
+    // useEffect(() => {});
+    // useEffect(() => {});
+
+    let { id } = useParams();
+    let history = useHistory();
+
+    let [amAlert, amAlert변경] = useState(true);
+
+    let params = parseInt(props.shoes[id].id);
+
+    return(
+        <div className="container">
+            <박스>
+                {/* <제목 색상="blue">안녕하세요1</제목>
+                <제목 색상={'red'}>안녕하세요2</제목> */}
+                {/* <제목 className="red">안녕하세요</제목> */}
+                {amAlert === true ? 
+                    <div className="my-alert">
+                        <p>재고가 얼마 남지 않았습니다</p>
+                    </div>
+                 : null
+                }
+            </박스>
+          <div className="row">
+            <div className="col-md-6">
+              <img src={ `https://codingapple1.github.io/shop/shoes${params+1}.jpg` } width="100%" />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">{props.shoes[params].title}</h4>
+              <p>{props.shoes[params].content}</p>
+              <p>{props.shoes[params].price}원</p>
+              <button className="btn btn-danger">주문하기</button> 
+              <button className="btn btn-danger" onClick={ () => {
+                //   history.push('/');
+                  history.goBack();
+              } }>뒤로가기</button> 
+            </div>
+          </div>
+        </div> 
+    )
+}
+
+export default Detail;
