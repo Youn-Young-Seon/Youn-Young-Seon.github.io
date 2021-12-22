2cd5f887c800db8d9123c9952e93ec91e045cc6c
Youn-Young-Seon <szdbsyyzs@naver.com>
2021-12-22 17:58:53 +0900

    apple NodeJs chap.3 chat working...

--- NodeJs/todoapp/server.js
+++ NodeJs/todoapp/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}))
+
 const MongoClient = require('mongodb').MongoClient;
 
 app.set('view engine', 'ejs');
@@ -12,6 +13,8 @@ const methodOverride = require('method-override');
 app.use(methodOverride('_method'))
 
 require('dotenv').config();
+
+const {ObjectId} = require('mongodb');
 
 // var db;
 // MongoClient.connect('mongodb+srv://admin:admin123@cluster0.99oln.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function(에러, client){
@@ -64,37 +67,12 @@ app.get('/write', function(req, res){
     res.render('write.ejs')
 })
 
-app.post('/add', function(req, res){
-    db.collection('counter').findOne({name: '게시물갯수'}, function(error, result){
-        console.log(result.totalPost);
-        var 총게시물갯수 = result.totalPost;
-        
-        db.collection('post').insertOne({_id: (총게시물갯수 + 1), 제목: req.body.title, 날짜: req.body.date}, function(error, result){
-            console.log('성공');
-            
-            db.collection('counter').updateOne({name: '게시물갯수'},{ $inc: {totalPost: 1} },function(error, result){
-                if(error) return console.log(error);
-            })
-        })    
-    });
-    res.redirect('/list')
-})
-
 app.get('/list', function(req, res){    
     db.collection('post').find().toArray(function(error, result){
         console.log(result);
         res.render('list.ejs', {posts: result});
     });
 
-})
-
-app.delete('/delete', function(req, res){
-    req.body._id = parseInt(req.body._id);
-    db.collection('post').deleteOne(req.body, function(e, result){
-        console.log('삭제완료');
-        res.status(200).send({message: '성공했습니다'}); // 응답코드
-        
-    })
 })
 
 app.get('/detail/:id', function(req, res){
@@ -150,7 +128,8 @@ function 로그인했니(req, res, next){
     if(req.user){
         next()
     }else{
-        res.send('로그인안하셧는데요?');
+        // res.send('로그인안하셧는데요?');
+        res.redirect('/login')
     }
 }
 
@@ -182,6 +161,43 @@ passport.deserializeUser(function(아이디, done){
     })
 })
 
+app.post('/register', function(req, res){    
+    db.collection('login').insertOne({ id: req.body.id, pw: req.body.pw}, function(error, result){
+        res.redirect('/')
+    })
+})
+
+app.post('/add', function(req, res){
+    db.collection('counter').findOne({name: '게시물갯수'}, function(error, result){
+        console.log(result.totalPost);
+        var 총게시물갯수 = result.totalPost;
+
+        var 저장할거 = {_id: (총게시물갯수 + 1), 작성자: req.user._id, 제목: req.body.title, 날짜: req.body.date}
+
+        db.collection('post').insertOne(저장할거, function(error, result){
+            console.log('성공');
+            
+            db.collection('counter').updateOne({name: '게시물갯수'},{ $inc: {totalPost: 1} },function(error, result){
+                if(error) return console.log(error);
+            })
+        })    
+    });
+    res.redirect('/list')
+})
+
+app.delete('/delete', function(req, res){
+    req.body._id = parseInt(req.body._id);
+
+    var 삭제할데이터 = { _id: req.body._id, 작성자: req.user._id}
+
+    db.collection('post').deleteOne(삭제할데이터, function(e, result){
+        console.log('삭제완료');
+        if (result) return console.log(result);
+        res.status(200).send({message: '성공했습니다'}); // 응답코드
+        
+    })
+})
+
 
 app.get('/search', (req, res) => {
     var 검색조건 = [
@@ -201,5 +217,72 @@ app.get('/search', (req, res) => {
     db.collection('post').aggregate(검색조건).toArray(function(error, result){
         console.log(result);
         res.render('search.ejs', {posts: result});
+    })
+})
+
+app.use('/shop', require('./routes/shop.js'));
+app.use('/board/sub', require('./routes/board.js'))
+
+
+let multer = require('multer');
+// var storage = multer.memoryStorage({}) // memory 상에 저장 (휘발성)
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './public/image')
+    },
+    filename: function(req, file, cb){
+        cb(null, file.originalname)
+    },
+    filefilter: function(req, file, cb){
+
+    }
+});
+
+var upload = multer({storage: storage});
+
+app.get('/upload', function(req, res){
+    res.render('upload.ejs')
+})
+
+app.post('/upload', upload.single('profile'), function(req, res){
+    res.send('업로드완료');
+});
+
+app.get('/image/:imageName', function(req, res){
+    res.sendFile(__dirname + '/public/image/' + req.params.imageName);
+})
+
+
+app.get('/chat', function(req, res){    
+    db.collection('chatroom').find().toArray(function(error, result){
+        // for(let i=0; i<result.length; i++){
+        //     for(let j=0; i<result.member[j]; j++){
+        //         if(result.member[j] == req.user._id){
+        //             chatArr += 
+        //         }
+        //     }
+        // }
+        let chatArr = result.filter((a) => {
+            for(let i=0; i<a.member.length; i++){
+                if(a.member[i] == req.user._id){
+                    return a;
+                }
+            }
+        })
+        console.log(chatArr);
+    })
+    res.render('chat.ejs');
+})
+
+app.post('/chat', 로그인했니, function(req, res){    
+    let data = {
+        title: req.body.title,
+        member: [ObjectId(req.body.writer), req.user._id],
+        date: new Date()
+    }
+    db.collection('chatroom').insertOne(data, function(error, result){
+        if(result){
+            res.redirect('/chat')
+        }
     })
 })
